9889a4bb5a12f5656232a3382b1abb4dde598aa3
broadrock4 <burdi009@cougars.csusm.edu>
2020-10-23 21:02:18 -0700

    User Sign Up commit

---
+++ User Register/userRegister.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+function chkUsername(){
+	var name = document.getElementById("username").value;
+	if (name.length <= 1){
+		alert("Error: Username is required and must be longer then 3 characters.");
+		return false
+	}
+	else{
+		return true;
+	}
+	
+}
+
+function chkPasswords(){
+	var pass1 = document.getElementById("pwd1").value;
+	var pass2 = document.getElementById("pwd2").value;
+	
+	var test = pass1.search(/[A-Z]/)
+	if(test != 0){
+		alert("Password Must contain at least one Capital letter.");
+		return false;
+	}
+	if(pass1.length <= 5 || pass2.length <= 5){
+		alert("Error Password must be larger then 5 characters.")
+		return false
+	}
+	if(pass1!=pass2){
+		alert("Error: Passwords must match.");
+		return false;
+	}
+	else{
+		alert("Account Created!");
+		return true;
+	}
+}
---
+++ User Register/userRegisterR.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+
+document.getElementById("username").onchange = chkUsername;
+document.getElementById("submitButton").onclick = chkPasswords;
