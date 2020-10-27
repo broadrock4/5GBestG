98b167904fbbd81777a2f5309b19e4d10bbd6fe8
broadrock4 <burdi009@cougars.csusm.edu>
2020-10-27 14:48:56 -0700

    12/27 First commit

--- Admin_Register/adminRegister.js
+++ Admin_Register/adminRegister.js
@@ -15,18 +15,23 @@ function chkUsername(){
 function chkPasswords(){
 	var pass1 = document.getElementById("pwd1").value;
 	var pass2 = document.getElementById("pwd2").value;
+	var pin = document.getElementById("adminPIN").value
 	
 	var test = pass1.search(/[A-Z]/)
 	if(test != 0){
-		alert("Password Must contain at least one Capital letter.");
+		alert("Error: Password Must contain at least one Capital letter.");
 		return false;
 	}
 	if(pass1.length <= 5 || pass2.length <= 5){
-		alert("Error Password must be larger then 5 characters.")
+		alert("Error: Password must be larger then 5 characters.")
 		return false
 	}
 	if(pass1!=pass2){
 		alert("Error: Passwords must match.");
+		return false;
+	}
+	if(pin.length == 0){
+		alert("Error: A Pin is required. Please add Admin Validation PIN.");
 		return false;
 	}
 	else{
