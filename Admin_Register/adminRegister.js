a0043a5e76ea1c77e121235e3d5cd23e3f3ea83f
broadrock4 <burdi009@cougars.csusm.edu>
2020-10-27 17:21:31 -0700

    10/27 Second commit

--- Admin_Register/adminRegister.js
+++ Admin_Register/adminRegister.js
@@ -1,43 +1,45 @@
 // JavaScript Document 
 //Event Handling Functions
-function chkUsername(){
-	var name = document.getElementById("username").value;
-	if (name.length <= 1){
-		alert("Error: Username is required and must be longer then 3 characters.");
-		return false
-	}
-	else{
-		return true;
-	}
-	
+function chkUsername() {
+  var name = document.getElementById("username").value;
+  if (name.length <= 1) {
+    alert("Error: Username is required and must be longer then 3 characters.");
+    return false
+  } else {
+    return true;
+  }
+
 }
 
-function chkPasswords(){
-	var pass1 = document.getElementById("pwd1").value;
-	var pass2 = document.getElementById("pwd2").value;
-	var pin = document.getElementById("adminPIN").value
+function chkPasswords() {
+  var pass1 = document.getElementById("pwd1").value;
+  var pass2 = document.getElementById("pwd2").value;
+  var pin = document.getElementById("adminPIN").value
+  var usName = document.getElementById("username").value;
 	
-	var test = pass1.search(/[A-Z]/)
-	if(test != 0){
-		alert("Error: Password Must contain at least one Capital letter.");
+	if(pass1.length==0 || pass2.length==0 || usName==0 || pin.length == 0){
+		alert("Error: Username, password, confirm password, and PIN required.")
 		return false;
 	}
-	if(pass1.length <= 5 || pass2.length <= 5){
-		alert("Error: Password must be larger then 5 characters.")
-		return false
-	}
-	if(pass1!=pass2){
-		alert("Error: Passwords must match.");
-		return false;
-	}
-	if(pin.length == 0){
-		alert("Error: A Pin is required. Please add Admin Validation PIN.");
-		return false;
-	}
-	else{
-		alert("Account Created!");
-		return true;
-	}
+
+  var test = pass1.search(/[A-Z]/)
+  if (test != 0) {
+    alert("Error: Password Must contain at least one Capital letter.");
+    return false;
+  }
+  if (pass1.length <= 5 || pass2.length <= 5) {
+    alert("Error: Password must be larger then 5 characters.")
+    return false
+  }
+  if (pass1 != pass2) {
+    alert("Error: Passwords must match.");
+    return false;
+  }
+  
+  else {
+    alert("Account Created!");
+    return true;
+  }
 }
 
 //Dynamic Html Functions
--- User_Register/userRegister.js
+++ User_Register/userRegister.js
@@ -1,8 +1,8 @@
 // JavaScript Document 
 //Event Handling Functions
 function chkUsername(){
-	var name = document.getElementById("username").value;
-	if (name.length <= 1){
+	var usName = document.getElementById("username").value;
+	if (usName.length <= 3){
 		alert("Error: Username is required and must be longer then 3 characters.");
 		return false
 	}
@@ -15,7 +15,12 @@ function chkUsername(){
 function chkPasswords(){
 	var pass1 = document.getElementById("pwd1").value;
 	var pass2 = document.getElementById("pwd2").value;
+	var usName = document.getElementById("username").value;
 	
+	if(pass1.length==0 || pass2.length==0 || usName==0){
+		alert("Error: Username, password and confirm password required.")
+		return false;
+	}
 	var test = pass1.search(/[A-Z]/)
 	if(test != 0){
 		alert("Password Must contain at least one Capital letter.");
